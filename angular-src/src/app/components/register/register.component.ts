1fdd28165c705bc3557bc81416e6036aa71b584f
prajakt <prajakt@vishnu.com>
2019-01-20 21:14:27 +0530

    Client side validation for required and email on register

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+name: String;
+username:String;
+email:String;
+password:String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user = {
+      name : this.name,
+      username : this.username,
+      email : this.email,
+      password : this.password
+    }
+
+  console.log('Validation started');
+
+  //required fields
+  if(!this.validateService.validateRegister(user)){
+    console.log('Please fill in all fields');
+    return false;
+  }
+
+  //validate email
+  if(!this.validateService.validateEmail(user.email)){
+    console.log('Please enter valid email');
+    return false;
+
+    }
+
+    console.log('Validation completed');
   }
 
 }
