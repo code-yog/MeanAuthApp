85f3fee62f7eb0b3c06469ec8d789b343f5b1d76
prajakt <prajakt@vishu.com>
2019-02-16 17:56:45 +0530

    changed connection string

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //database: 'mongodb://localhost:27017/meanauth',
-    database: 'mongodb+srv://codeyogee:P%40ssC0de@cluster0-uv6cz.mongodb.net/test?retryWrites=true',
+    database: 'mongodb://codeyogee:C0deW0rd@cluster0-shard-00-00-uv6cz.mongodb.net:27017,cluster0-shard-00-01-uv6cz.mongodb.net:27017,cluster0-shard-00-02-uv6cz.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
     secret: 'yoursecret'
 }
